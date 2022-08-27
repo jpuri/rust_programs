9ee35acb426fe2538683cfaa3bca15a1e6733ec1
jpuri <jyotipuri@gmail.com>
2022-08-27 19:04:21 +0530

    Multiline comment

--- array/src/main.rs
+++ array/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     // This is single line comment
     let a = [1, 2, 3, 4, 5];
 
+    /*
+     * This is multiline comment
+     */
     let first = a[0];
     println!("first = {}", first);
 
